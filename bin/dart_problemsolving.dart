c3797123f6e860bc6586689cae491165ff361d96
amrelwekeel <amrgamalelwekeel.com>
2022-03-21 00:39:28 +0200

    first Problem

--- bin/dart_problemsolving.dart
+++ bin/dart_problemsolving.dart
@@ -1,5 +1,21 @@
 import 'package:dart_problemsolving/dart_problemsolving.dart' as dart_problemsolving;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_problemsolving.calculate()}!');
+
+  // Take a list, say for example this one:
+  // a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  //and write a program that prints out all the elements of the list that are less than 5.
+
+  ///Solution
+  //    List<int> a =[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  //    for(var i in a ){
+  //       if(i< 5 ){
+  //          print(i);
+  //       }
+  //    }
+
+
+
+
+
 }
