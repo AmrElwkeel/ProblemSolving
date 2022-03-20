380f3d777918fd8039ac90706ac3db2a54ac880a
amrelwekeel <amrgamalelwekeel.com>
2022-03-21 01:19:19 +0200

    Three Problem

--- bin/dart_problemsolving.dart
+++ bin/dart_problemsolving.dart
@@ -29,6 +29,25 @@ void main(List<String> arguments) {
   //      }
   //   }
 
+///and write a program that returns a list that contains only the elements that are common between them (without duplicates).
+  ///Make sure your program works on two lists of different sizes.
+  ///Solution
+
+  // List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  //
+  // List<int> b = [1, 2, 3, 4, 5, 6, 7,55, 8, 9, 10, 11, 12, 13];
+  // var li = [];
+  //
+  //  for(var i in a ){
+  //    for(var j in b){
+  //      if(i == j ){
+  //        li.add(i);
+  //      }
+  //    }
+  //  }
+  // print(li.toList());
+
+
 
 
 
