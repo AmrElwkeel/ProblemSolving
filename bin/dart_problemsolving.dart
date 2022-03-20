89cecf10a8f0eb1b7faca0adcc4841e8b64e1874
amrelwekeel <amrgamalelwekeel.com>
2022-03-21 00:34:03 +0200

    createProject

---
+++ bin/dart_problemsolving.dart
@@ -0,0 +1,5 @@
+import 'package:dart_problemsolving/dart_problemsolving.dart' as dart_problemsolving;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_problemsolving.calculate()}!');
+}
---
+++ test/dart_problemsolving_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_problemsolving/dart_problemsolving.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
